a84eb684d5a314c0dad2ef34683df104eaea12f5
Adedoyin <isradeoluwa@gmail.com>
2020-01-07 16:24:30 +0100

    Setting the OnclickListener Onymously

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -15,19 +15,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button btnRight = findViewById(R.id.btnRight);
-        View.OnClickListener myClickListener = new View.OnClickListener() {
+        btnRight.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (v.getId() == R.id.btnRight){
-                    Log.i("MyApp", "btn True is tapped now!!!");
-                } else if (v.getId() == R.id.btnWrong){
-                    Log.i("MyApp", "btn Wrong is tapped now!!!");
-                }
+                Log.i("MyApp", "btn True is tapped now!!!");
             }
-        };
+        });
 
-        btnRight.setOnClickListener(myClickListener);
         Button btnWrong = findViewById(R.id.btnWrong);
-        btnWrong.setOnClickListener(myClickListener);
+        btnWrong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("MyApp", "btn Wrong is tapped now!!!");
+            }
+        });
+//
+//        View.OnClickListener myClickListener = new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (v.getId() == R.id.btnRight){
+//                    Log.i("MyApp", "btn True is tapped now!!!");
+//                } else if (v.getId() == R.id.btnWrong){
+//                    Log.i("MyApp", "btn Wrong is tapped now!!!");
+//                }
+//            }
+//        };
+//
+//        btnRight.setOnClickListener(myClickListener);
+//        Button btnWrong = findViewById(R.id.btnWrong);
+//        btnWrong.setOnClickListener(myClickListener);
     }
 }
