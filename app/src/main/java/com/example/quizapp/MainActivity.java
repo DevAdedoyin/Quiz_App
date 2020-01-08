727487d7d2d4aac24c64acf4a011e08ad9d7f10b
Adedoyin <isradeoluwa@gmail.com>
2020-01-08 16:42:59 +0100

    Working on the controller(MainActivity) to display questions to the TextView

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -6,16 +6,43 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextQuestion ;
+    private Button btnRight;
+    private Button btnWrong;
+    private int mQuestionIndex;
+
+    private  QuizModel[] questionCollection = new QuizModel[]{
+            new QuizModel(R.string.q1, true),
+            new QuizModel(R.string.q2, false),
+            new QuizModel(R.string.q3, true),
+            new QuizModel(R.string.q4, false),
+            new QuizModel(R.string.q5, true),
+            new QuizModel(R.string.q6, false),
+            new QuizModel(R.string.q7, true),
+            new QuizModel(R.string.q8, false),
+            new QuizModel(R.string.q9, true),
+            new QuizModel(R.string.q10, false),
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnRight = findViewById(R.id.btnRight);
+        mTextQuestion = findViewById(R.id.txtView1);
+
+        QuizModel q1 = questionCollection[mQuestionIndex];
+
+        mTextQuestion.setText(q1.getQuestion());
+
+        btnRight = findViewById(R.id.btnRight);
         btnRight.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -25,8 +52,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button btnWrong = findViewById(R.id.btnWrong);
-
+        btnWrong = findViewById(R.id.btnWrong);
         btnWrong.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -35,23 +61,5 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
-        QuizModel myQuizModel = new QuizModel(R.string.q1, true);
-
-//
-//        View.OnClickListener myClickListener = new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                if (v.getId() == R.id.btnRight){
-//                    Log.i("MyApp", "btn True is tapped now!!!");
-//                } else if (v.getId() == R.id.btnWrong){
-//                    Log.i("MyApp", "btn Wrong is tapped now!!!");
-//                }
-//            }
-//        };
-//
-//        btnRight.setOnClickListener(myClickListener);
-//        Button btnWrong = findViewById(R.id.btnWrong);
-//        btnWrong.setOnClickListener(myClickListener);
     }
 }
