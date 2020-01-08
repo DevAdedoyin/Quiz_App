b9cd2ae35b539a8979e4c71beed3959581c4c928
Adedoyin <isradeoluwa@gmail.com>
2020-01-08 19:53:14 +0100

    Worked on the "progress bar" and the "user stats TextView"

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,10 +16,15 @@ public class MainActivity extends AppCompatActivity {
 
     // Declaring instance variables(fields)
     private TextView mTextQuestion ;
+    private TextView mTextStats;
     private Button btnRight;
     private Button btnWrong;
+    private ProgressBar mProgressBar;
+
     private int mQuestionIndex;
     private int mQuizQuestion;
+
+    private int mUserScore;
 
     // An array Instance variable of type QuizModel, which contains the needed quiestions
     private  QuizModel[] questionCollection = new QuizModel[]{
@@ -34,10 +40,15 @@ public class MainActivity extends AppCompatActivity {
             new QuizModel(R.string.q10, false),
     };
 
+    final int USER_PROG = (int) Math.ceil(100 / questionCollection.length);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextStats = findViewById(R.id.txtView2);
+        mProgressBar = findViewById(R.id.prog_bar);
 
         //Initializing the mTextQustion Variable
         mTextQuestion = findViewById(R.id.txtView1);
@@ -74,6 +85,8 @@ public class MainActivity extends AppCompatActivity {
         mQuestionIndex = (mQuestionIndex + 1) % 10;
         mQuizQuestion = questionCollection[mQuestionIndex].getQuestion();
         mTextQuestion.setText(mQuizQuestion);
+        mProgressBar.incrementProgressBy(USER_PROG);
+        mTextStats.setText(mUserScore + "");
 
     }
 
@@ -82,6 +95,7 @@ public class MainActivity extends AppCompatActivity {
         boolean currentQuestionAnswer = questionCollection[mQuestionIndex].isAnswer();
         if (currentQuestionAnswer == userGuess){
             Toast.makeText(getApplicationContext(), R.string.correct_text, Toast.LENGTH_LONG).show();
+            mUserScore+=1;
         } else {
             Toast.makeText(getApplicationContext(), R.string.incorrect_text, Toast.LENGTH_LONG).show();
         }
